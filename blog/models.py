c3982a1d085144f465b7c2ea391f07eb1c739776
EugenioUrbano <urbanoe348@gmail.com>
2024-09-02 16:44:28 -0300

    ate agr deu tudo certo

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Genero(models.Model):
+    nome = models.CharField(max_length=30)
+    imagem = models.ImageField()
+    descricao_1 = models.TextField()
+    descricao_2 = models.TextField()
+    descricao_3 = models.TextField()
+    descricao_4 = models.TextField()
+    descricao_5 = models.TextField()
+    descricao_6 = models.TextField()
+    descricao_7 = models.TextField()
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def index(request):
+    generos= {"titulo": "titulo" }
     return render(request, 'index.html')
